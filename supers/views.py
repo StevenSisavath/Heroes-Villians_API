c4d5d74dec0e9c29ccd09aa1f487bf05607c04a9
Steven Sisavath <stevenlsisavath@gmail.com>
2022-11-15 09:38:42 -0600

    Updated Serializer in Supers File + Added POST and GET Request to Views File + Updated to GET Request to Include Parameters

--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Super
+from .serializers import SuperSerializer
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def cars_list(request):
+
+    if request.method == 'GET':
+        super_type = request.query_params.get("supertype")
+        supers = Super.objects.all()
+        if super_type:
+            queryset = queryset.filter(super_type=super_type)
+        serializer = SuperSerializer(supers, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = SuperSerializer(data = request.data)      
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status = status.HTTP_201_CREATED)
